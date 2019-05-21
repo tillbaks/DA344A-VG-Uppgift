44245324d7eac854768b7d36840cc84d2be9618f
Johannes Blüml <johannes@bluml.se>
2019-05-21 14:27:08 +0200

    Implemented local storage and proxy for API so it works from browser because of CORS https://github.com/tillbaks/DA344A-VG-Uppgift/issues/3

--- src/cat-facts-api.js
+++ src/cat-facts-api.js
@@ -1,86 +1,91 @@
-const axios = require('axios')
+function CatFactsApi(localStorageName = 'catFactsApi.animalTypes', factsToRetrieve = 10) {
 
-const api = 'https://cat-fact.herokuapp.com'
-const factsToRetrieve = 10
+  // API needs proxy since cat-fact api does not allow CORS
+  const api = 'https://cors-anywhere.herokuapp.com/https://cat-fact.herokuapp.com'
+  let availableFacts = []
+  let animalTypes = JSON.parse(window.localStorage.getItem(localStorageName))
+  if (animalTypes === null) {
+    animalTypes = { cat: true, dog: false, horse: false }
+  }
 
-let animalTypes = {cat: true, dog: false, horse: false}
-let availableFacts = []
-
-/**
- * Updates the facts stored in memory to reduce http requests
- */
-async function updateFacts() {
+  /**
+   * Updates the facts stored in memory to reduce http requests
+   */
+  async function updateFacts() {
     const result = await axios.get(`${api}/facts/random`, {
-        params: {
+      params: {
         amount: factsToRetrieve,
         animal_type: getAnimalTypesCommaSeparated()
-        }
+      }
     })
 
     result.data.forEach(fact => availableFacts.push(fact))
-}
+  }
 
-function clearFacts() {
+  function clearFacts() {
     availableFacts = []
-}
+  }
 
-/**
- * @returns {string} Comma separated list of selected animal types, if all are unselected all will be returned so you will never get an empty string
- */
-function getAnimalTypesCommaSeparated() {
+  /**
+   * @returns {string} Comma separated list of selected animal types, if all are unselected all will be returned so you will never get an empty string
+   */
+  function getAnimalTypesCommaSeparated() {
     let types = Object.entries(animalTypes)
-        .filter(([key, value]) => value)
-        .join(',')
+      .filter(([key, value]) => value)
+      .join(',')
 
     if (types === '') {
-        types = Object.keys(animalTypes).join(',')
+      types = Object.keys(animalTypes).join(',')
     }
 
     return types
-}
+  }
 
-/**
- * Retrieves a random fact
- * 
- * @returns {Object} A fact object
- */
-async function getRandomFact() {
+  /**
+   * Retrieves a random fact
+   * 
+   * @returns {Object} A fact object
+   */
+  async function getRandomFact() {
     if (availableFacts == 0) {
-        await updateFacts()
+      await updateFacts()
     }
 
     return availableFacts.pop()
-}
+  }
 
-/**
- * Select which animal types are wanted when retrieving random facts
- * 
- * @param {Object}  object          Object containing animal types
- * @param {boolean} object.cat      Facts about cats
- * @param {boolean} object.dog      Facts about dogs
- * @param {boolean} object.horse    Facts about horses
- */
-function setAnimalTypes({cat, dog, horse}) {
+  /**
+   * Select which animal types are wanted when retrieving random facts
+   * 
+   * @param {Object}  object          Object containing animal types
+   * @param {boolean} object.cat      Facts about cats
+   * @param {boolean} object.dog      Facts about dogs
+   * @param {boolean} object.horse    Facts about horses
+   */
+  function setAnimalTypes({ cat, dog, horse }) {
     // Clear so new animal types are honored when retrieving facts
     clearFacts()
 
     if (cat !== undefined) {
-        animalTypes.cat = cat
+      animalTypes.cat = cat
     }
     if (dog !== undefined) {
-        animalTypes.dog = dog
+      animalTypes.dog = dog
     }
     if (horse !== undefined) {
-        animalTypes.horse = horse
+      animalTypes.horse = horse
     }
-}
 
-function getAnimalTypes() {
+    window.localStorage.setItem(localStorageName, JSON.stringify(animalTypes))
+  }
+
+  function getAnimalTypes() {
     return animalTypes
-}
+  }
 
-module.exports = {
-    getRandomFact, 
-    setAnimalTypes, 
+  return {
+    getRandomFact,
+    setAnimalTypes,
     getAnimalTypes
+  }
 }
