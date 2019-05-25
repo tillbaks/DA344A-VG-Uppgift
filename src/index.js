1aab900f83b6c2889936faee908bed5f4ef62baf
Johannes Blüml <johannes@bluml.se>
2019-05-25 23:37:17 +0200

    Animation transitions everywhere completed https://github.com/tillbaks/DA344A-VG-Uppgift/issues/4

--- src/index.js
+++ src/index.js
@@ -4,37 +4,46 @@ import CatFactsApi from './CatFactsApi.js'
 // Constants
 
 const catFactApi = CatFactsApi()
+const animate = Animations()
 const elNextFact = document.querySelector('.next-fact')
 const elFact = document.querySelector('article.fact')
-const elLoading = document.querySelector('article.loading')
-const elError = document.querySelector('article.error')
 const elButtonCat = document.querySelector('.button.cat')
 const elButtonDog = document.querySelector('.button.dog')
 const elButtonHorse = document.querySelector('.button.horse')
+const state = {
+  visibleView: '.no-view-selected.yet'
+}
 
 // Functions
 
-function showView (element) {
-  elFact.classList.add('hidden')
-  elError.classList.add('hidden')
-  elLoading.classList.add('hidden')
-  element.classList.remove('hidden')
+async function changeView (selector) {
+  await animate.fadeOut(state.visibleView)
+  if (selector === 'article.fact') {
+    document.querySelector(selector).style.opacity = 1
+    await animate.randomText(selector)
+  } else {
+    await animate.fadeIn(selector)
+  }
+  state.visibleView = selector
 }
 
-async function loadNextFact () {
-  showView(elLoading)
+async function loadNextFact (initialLoad) {
+  if (!initialLoad) animate.slideOutRight('nav.next-fact')
+  await changeView('article.loading')
   try {
     const fact = await catFactApi.getRandomFact()
     elFact.innerHTML = fact.text
-    showView(elFact)
-    Animations().randomText('article.fact')
+    await changeView('article.fact')
   } catch (error) {
     console.error(error)
-    showView(elError)
+    await changeView('article.error')
+  } finally {
+    await animate.slideInRight('nav.next-fact')
   }
 }
 
-function setAnimalType (type, element) {
+async function setAnimalType (type, element) {
+  await animate.wobbleLeft('nav.animal-type-chooser')
   const isSet = element.classList.contains('active')
   const result = {}
   result[type] = !isSet
@@ -48,15 +57,21 @@ function setAnimalType (type, element) {
 
 // Event Listeners
 
-elNextFact.addEventListener('click', event => loadNextFact(elFact))
+elNextFact.addEventListener('click', event => loadNextFact())
 elButtonCat.addEventListener('click', event => setAnimalType('cat', elButtonCat))
 elButtonDog.addEventListener('click', event => setAnimalType('dog', elButtonDog))
 elButtonHorse.addEventListener('click', event => setAnimalType('horse', elButtonHorse))
 
 // Set initial state
 
-loadNextFact(elFact)
 const initialAnimalTypes = catFactApi.getAnimalTypes()
 if (initialAnimalTypes.cat) elButtonCat.classList.add('active')
 if (initialAnimalTypes.dog) elButtonDog.classList.add('active')
 if (initialAnimalTypes.horse) elButtonHorse.classList.add('active')
+
+
+// Navigation slides in from sides
+animate.slideInPage('header', 'footer', 'nav.animal-type-chooser', 'nav.next-fact')
+  .then(() => loadNextFact(true))
+
+window.changeView = (sel) => changeView(sel)
